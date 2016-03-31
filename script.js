09dee6354c673a11d6360bc3a1c041c563d455a5
erezalgazi <erezalgazi@gmail.com>
2016-03-31 11:53:05 +0300

    finished updating cart

--- script.js
+++ script.js
@@ -1,55 +1,72 @@
-// an array with all of our cart items
-var cart = [];
-var total = 0;
-// var replaced = $('.total').html().replace('0', total);
-// $('.total').html(replaced);
+var shoppingCartApp = function () {
 
-// var updateCart = function () {
-//   // TODO: finish
-// }
-
-
-var addItem = function (item) {
-    total = total + item.price;
-    var source = $('#result-template').html();
-
-    var template = Handlebars.compile(source);
-
-    var newHTML = template(item);
-
-    $('.cart-list').append(newHTML);
-    $('.total').html(total);
+	// an array with all of our cart items
+	var cart = [];
+	var total = 0;
+	// var replaced = $('.total').html().replace('0', total);
 	// $('.total').html(replaced);
 
-}
+	var updateCart = function (item) {
 
-var clearCart = function () {
-	cart = [];
-	total = 0;
-	$('.cart-list').empty();
-	$('.total').html(total);	
-}
+	    var source = $('#result-template').html();
 
-$('.view-cart').on('click', function () {
-	// var $clickedPost = $(currentPost).closest('.post');
-	$('.container').find('.shopping-cart').toggleClass('show');
+	    var template = Handlebars.compile(source);
+
+	    var newHTML = template(item);
+
+	    total = total + item.price;
+	    $('.cart-list').append(newHTML);
+	    $('.total').html(total);
+		// $('.total').html(replaced);
+	}
+
+
+	var addItem = function (item) {
+	    cart.push(item);
+	};
+
+	var clearCart = function () {
+		cart = [];
+		total = 0;
+		$('.cart-list').empty();
+		$('.total').html(total);	
+	};
+
+	var toggleShoppingCart = function () {
+			$('.container').find('.shopping-cart').toggleClass('show');
+	}; 
+
+	return {
+		addItem: addItem,
+		updateCart: updateCart,
+		clearCart: clearCart,
+		toggleShoppingCart: toggleShoppingCart
+	};
+};
+var app = shoppingCartApp();
+
+
+	$('.view-cart').on('click', function () {		
+ 	app.toggleShoppingCart(); 	
 });
+// $('.view-cart').on('click', function () {
+// 	// var $clickedPost = $(currentPost).closest('.post');
+// 	$('.container').find('.shopping-cart').toggleClass('show');
+// });
 
 $('.add-to-cart').on('click', function () {
   var name = $(this).closest('.card').data().name;
   var price = $(this).closest('.card').data().price; 
   var item = {
-  	name:name,
+  	name: name,
   	price: price
   }
-  cart.push(item);
-  console.log(cart);
-  addItem(item);
-  // updateCart();
+  app.addItem(item);
+  app.updateCart(item);
 });
 
 $('.clear-cart').on('click', function () {
-  clearCart();
+  app.clearCart();
 });
 
 // update the cart as soon as the page loads!
